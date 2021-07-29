d827c727f9b703824e630437ab21ea1137f9a1b8
David M. Fajardo <davidmf99@gmail.com>
2021-07-29 22:47:56 +0200

    Update navbar_box_shadow.js

--- public/scripts/navbar_box_shadow.js
+++ public/scripts/navbar_box_shadow.js
@@ -1,7 +1,7 @@
 // Box shadow on navbar at scrollpage
 window.addEventListener('scroll', (e) => {
 
-	var nav = document.getElementById('navboxshadow')
+	const nav = document.getElementById('navboxshadow')
 
 	if (window.pageYOffset > 0) {
 		nav.style.boxShadow = "rgb(77 77 77) 0px 0px 10px"
