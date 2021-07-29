9236d5490af654b5cfff08dfb89f254a5cc959a1
David M. Fajardo <davidmf99@gmail.com>
2021-07-29 23:42:16 +0200

    blog posts

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -8,17 +8,18 @@ import styles from '../styles/blog.module.scss'
 
 
 export default function Blog({ posts }) {
+	
 	return (
 		<Layout>
 			<div className="container">
 				<div className={styles.container}>
-					{ posts.map(({frontmatter: {title, description, date, image}, slug}, index: number) => (
+					{ posts.reverse().map(({frontmatter: {title, description, date, image}, slug}, index: number) => (
 						<Link href={'/post/[slug]'} as={`/post/${slug}`}>
 							<div className="animate__animated animate__fadeIn">
 								<div className={`row border rounded shadow-sm cursor-pointer ${index % 2 == 0 ? 'flex-row-reverse' : ''}`}>
 									<div className="col-lg-6 p-5">
 										<article key={title}>
-											<header>
+											<header className="mb-3">
 												<h3>{title}</h3>
 												<span>{date}</span>
 											</header>
