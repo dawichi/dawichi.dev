983c1479ec4ed32b634844a9673c5094a1f629c1
David M. Fajardo <davidmf99@gmail.com>
2021-10-18 16:35:12 +0200

    Update projects.tsx

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -5,7 +5,6 @@ import Projects from '../components/sections/Projects'
 const projects = () => {
 	return (
 		<Layout>
-			<div className="m-5">Projects page, incoming!</div>
 			<Projects />			
 		</Layout>
 	)
