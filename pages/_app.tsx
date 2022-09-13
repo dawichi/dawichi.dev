2e4156110d2c89117199e19e113c4d8a883cda5e
David M. Fajardo <davidmf99@gmail.com>
2022-09-13 12:12:56 +0200

    [ FEATURE ]: Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import Image from 'next/image'
 import Script from 'next/script'
 
 import 'tailwindcss/tailwind.css'
@@ -14,6 +13,22 @@ import 'styles/prism-onedark-theme.css'
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Script
+                strategy='lazyOnload'
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
+
+            <Script id="Google Analytics" strategy='lazyOnload'>
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
+
             <Head>
                 <title>Dawichi - software developer</title>
                 <link rel='icon' href='/favicon.ico' />
