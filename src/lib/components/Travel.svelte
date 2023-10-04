52535d0e6e26d15ddb1876b9639c0f70669200f0
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-10-04 17:32:13 +0200

    correct copy

--- src/lib/components/Travel.svelte
+++ src/lib/components/Travel.svelte
@@ -13,12 +13,12 @@
 
 <div class="mx-auto mt-40 max-w-5xl p-5">
     <h2 class="my-5 text-center text-2xl">
-        <i class="bi bi-geo-fill" /> Discovering the world
+        <i class="bi bi-geo-fill" /> Traveling the world
     </h2>
     <hr class="mx-auto h-1 w-3/4 rounded border-0 bg-zinc-700 dark:bg-zinc-200" />
 
     <p class="my-5 text-center">
-        I dream of traveling and knowing as many countries as possible. I would love to live in different countries throughout my life. Luckily, in tech
+        I dream of exploring and visiting as many countries as possible, living in different countries throughout my life. Luckily, in tech
         industry we have the ability to work remotely from where we want thanks to bleisure. What an amazing oportunity!
     </p>
 
