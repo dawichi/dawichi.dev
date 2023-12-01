54ac3f092c8c4f3053d0a2606fa14dd6abaa4798
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-12-01 11:31:04 +0100

    Reorder sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,16 @@
 
 <div class="animate__animated animate__fadeIn animate__faster">
     <Landing />
-    <Skills />
+
+    <div class="skew-y-6 bg-teal-500 p-10 text-center text-2xl text-white shadow shadow-teal-300 dark:bg-teal-800 dark:shadow-teal-500">
+        <h3>
+            Web development <br /> <i class="bi bi-code-slash" />
+        </h3>
+    </div>
+
     <About />
     <Timeline />
+    <Skills />
     <Languages />
     <Travel />
     <Contact />
