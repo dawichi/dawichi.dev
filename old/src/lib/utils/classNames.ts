50360d3e0a6ba64bd31058e1cf127c17414ec84a
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2024-04-18 16:26:36 +0200

    sections

--- old/src/lib/utils/classNames.ts
+++
@@ -1,8 +0,0 @@
-/**
- * ## ClassNames - Join class names
- * @param classes Array of classes
- * @returns A single string of classes
- */
-export function classNames(...classes: string[]): string {
-    return classes.filter(Boolean).join(' ')
-}
